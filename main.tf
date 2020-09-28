dd1766fca2f800f276a36dd9d9320813a5d84cc7
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-09-28 14:20:42 -0400

    added a cluster and a notebook

--- main.tf
+++ main.tf
@@ -41,3 +41,29 @@ resource "databricks_scim_user" "admin" {
   set_admin    = true
   default_roles = []
 }
+
+resource "databricks_cluster" "shared_autoscaling" {
+  cluster_name            = "${var.prefix}-Autoscaling-Cluster"
+  spark_version           = "7.3.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 5
+  autoscale {
+    min_workers = 1
+    max_workers = 4
+  }
+  library {
+  pypi {
+    package = "fbprophet==0.6"
+    // repo can also be specified here
+    }
+  }
+}
+
+resource "databricks_notebook" "notebook" {
+  content = base64encode("# Welcome to your Python notebook")
+  path = "/mynotebook"
+  overwrite = false
+  mkdirs = true
+  language = "PYTHON"
+  format = "SOURCE"
+}
