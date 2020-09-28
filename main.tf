3b7c102c5a10bd5663ddfd39c7b3dafb651d42f7
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-09-28 17:09:12 -0400

    remove job

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "databricks_cluster" "shared_autoscaling" {
 }
 
 resource "databricks_notebook" "notebook" {
-  content = base64encode("# Welcome to your Python notebook")
+  content = base64encode("print('Welcome to your Python notebook')")
   path = var.notebook_path
   overwrite = false
   mkdirs = true
@@ -73,25 +73,25 @@ resource "databricks_notebook" "notebook" {
   format = "SOURCE"
 }
 
-resource "databricks_job" "myjob" {
-    name = "Featurization"
-    timeout_seconds = 3600
-    max_retries = 1
-    max_concurrent_runs = 1
+// resource "databricks_job" "myjob" {
+//     name = "Featurization"
+//     timeout_seconds = 3600
+//     max_retries = 1
+//     max_concurrent_runs = 1
 
-    existing_cluster_id = databricks_cluster.shared_autoscaling.id
+//     existing_cluster_id = databricks_cluster.shared_autoscaling.id
 
-    notebook_task {
-        notebook_path = var.notebook_path
-    }
+//     notebook_task {
+//         notebook_path = var.notebook_path
+//     }
 
-    library {
-        pypi {
-            package = "fbprophet==0.6"
-        }
-    }
+//     library {
+//         pypi {
+//             package = "fbprophet==0.6"
+//         }
+//     }
 
-    email_notifications {
-        no_alert_for_skipped_runs = true
-    }
-}
+//     email_notifications {
+//         no_alert_for_skipped_runs = true
+//     }
+// }
